c9d6a22920712bc24112d333ba0842e5bd2b87fd
neth0024 <neth0024@flinders.edu.au>
2023-08-31 14:06:45 +0930

    Initial commit

---
+++ src/main/java/com/example/myhealthrecord/MyHealthRecordApplication.java
@@ -0,0 +1,13 @@
+package com.example.myhealthrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHealthRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHealthRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myhealthrecord/MyHealthRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myhealthrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHealthRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
