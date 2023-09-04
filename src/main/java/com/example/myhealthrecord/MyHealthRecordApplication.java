492d92ba15bc1b0c4c165cc318c8af810baa625e
neth0024 <neth0024@flinders.edu.au>
2023-09-04 11:57:03 +0930

    html commit2

--- src/main/java/com/example/myhealthrecord/MyHealthRecordApplication.java
+++ src/main/java/com/example/myhealthrecord/MyHealthRecordApplication.java
@@ -2,6 +2,8 @@ package com.example.myhealthrecord;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MyHealthRecordApplication {
@@ -10,6 +12,11 @@ public class MyHealthRecordApplication {
 		SpringApplication.run(MyHealthRecordApplication.class, args);
 	}
 
-
-
+	@Controller
+	public static class HomeController {
+		@GetMapping("/")
+		public String home() {
+			return "src/main/resources/templates/add-patient"; // Return the HTML file name without the extension
+		}
+	}
 }
